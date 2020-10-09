cc3c1a96a10bfa24fa46c16abc4190fddb174529
yetanothercheer <64053323+yetanothercheer@users.noreply.github.com>
2020-10-09 14:16:48 +0800

    Create ruby_interpreter_in_ruby.rb

---
+++ ruby_interpreter_in_ruby.rb
@@ -0,0 +1,4 @@
+# http://aosabook.org/en/500L/a-python-interpreter-written-in-python.html
+# https://stackoverflow.com/questions/38195290/how-to-run-the-bytecode-generated-by-ruby
+# http://www.atdot.net/yarv
+# http://rubyquiz.com/quiz100.html
