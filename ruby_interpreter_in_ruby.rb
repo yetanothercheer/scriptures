8e30f06c6d1652022493b66327a4a9fa9e0030a7
yetanothercheer <64053323+yetanothercheer@users.noreply.github.com>
2020-10-10 09:47:10 +0800

    Update ruby_interpreter_in_ruby.rb

--- ruby_interpreter_in_ruby.rb
+++ ruby_interpreter_in_ruby.rb
@@ -2,3 +2,4 @@
 # https://stackoverflow.com/questions/38195290/how-to-run-the-bytecode-generated-by-ruby
 # http://www.atdot.net/yarv
 # http://rubyquiz.com/quiz100.html
+# https://www.python.org/dev/peps/pep-0617
